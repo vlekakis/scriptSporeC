02d5ff574be2f01b7fc3cb03e1529a7141396111
vlekakis <lekakis@gmail.com>
2015-04-10 07:14:26 -0700

    runDelayThroughput: runner bash script for running delay/throuput experiment

--- runDelayThrouput.sh
+++ runDelayThrouput.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# A quick script to run delay/throughput experiments back to back
 python experimentRunner.py -b /Users/lex/Work/IdeaProjects/YCSB/bin/ycsb -i /Users/lex/Work/IdeaProjects/YCSB/sporeWorkloads/ -o edgeMultipleThreadsUnlimited -w update50read50 -c redis -f 8 80 800  -m load run loadSS runSS --thread 5;
-sleep 1;
+sleep 100;
 python experimentRunner.py -b /Users/lex/Work/IdeaProjects/YCSB/bin/ycsb -i /Users/lex/Work/IdeaProjects/YCSB/sporeWorkloads/ -o edgeMultipleThreadsTarget -w update50read50 -c redis -f 250  -m load run loadSS runSS --thread 5 --target 100 1000 5000 10000 20000;
